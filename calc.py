1f577c4f058e55c8a4ac61c4c3fe65c7d0416005
malik-pri <pri.malik93@gmail.com>
2024-03-11 10:55:09 +1100

    Use fancy-schemancy iterable unpacking

--- calc.py
+++ calc.py
@@ -1,8 +1,5 @@
 def compute(expression):
-    values = expression.split(' ')
-    num0 = int(values[0])
-    operator = values[1]
-    num1 = int(values[2])
+    num0, operator, num1 = expression.split(' ')
     if operator == '+':
         return num0 + num1
     elif operator == '-':
